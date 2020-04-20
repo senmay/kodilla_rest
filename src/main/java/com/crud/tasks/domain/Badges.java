c3826193a68e413c3de6768b0a6a0d74f6ea9e3b
senmay <dominik.szarzynski@gmail.com>
2020-04-20 21:23:42 +0200

    22.3

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,19 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Badges {
+
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachmentsByType;
+
+}
--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -5,24 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import java.util.List;
 
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class TrelloListDto {
+    @JsonProperty("id")
+
+    private String id;
 
     @JsonProperty("name")
     private String name;
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
-
+    @JsonProperty("closed")
+    private boolean isClosed;
 }
